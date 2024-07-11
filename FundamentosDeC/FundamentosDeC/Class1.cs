aa95422793913f831a1853df33bee614e41aa77c
Dobertin <robertohuacca@gmail.com>
2024-07-11 13:03:27 -0500

    Fin del proyecto de pruebas

--- FundamentosDeC/FundamentosDeC/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FundamentosDeC
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ FundamentosDeC/FundamentosDeC/LibreriaPractica.cs
@@ -0,0 +1,20 @@
+﻿namespace FundamentosDeC
+{
+    public class LibreriaPractica
+    {
+        double mayc = 234.12;
+        public int hola(double x, double y)
+        {
+            Convert.ToInt32(x);
+            int.Parse(mayc.ToString());
+            if (mayc.ToString() == "")
+            {
+                Math.Pow(mayc, 2);
+                //  Array.Sort()
+            }
+            else if (mayc.ToString() == "")
+            { }
+            return 0;
+        }
+    }
+}
