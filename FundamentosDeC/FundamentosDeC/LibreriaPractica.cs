62dd03516856e499c9b9737cd3590abd1121ccbc
Dobertin <robertohuacca@gmail.com>
2024-07-12 08:33:02 -0500

    metodo fibonacci

--- FundamentosDeC/FundamentosDeC/LibreriaPractica.cs
+++ FundamentosDeC/FundamentosDeC/LibreriaPractica.cs
@@ -17,4 +17,24 @@
             return 0;
         }
     }
+    public class LibreriaPractica2
+    {
+        public int Fibonacci(int n)
+        {
+            int result = Fibonacci(5);
+            Console.WriteLine(result);
+            int n1 = 0;
+            int n2 = 1;
+            int sum;
+
+            for (int i = 2; i <= n; i++)
+            {
+                sum = n1 + n2;
+                n1 = n2;
+                n2 = sum;
+            }
+
+            return n == 0 ? n1 : n2;
+        }
+    }
 }
